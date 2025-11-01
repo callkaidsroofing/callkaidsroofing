142c03baed660e3da77efb8cf64d6b0339e2420c
callkaidsroofing <axemaster321@gmail.com>
2025-11-02 03:20:37 +1100

    chore(mkf): fix workflow path; add index; restrict commit to docs/**; cleanup cache

--- supabase/migrations/20250913212103_3429fa9d-f125-4c4c-ad1c-23b9041cad93.sql
+++
@@ -1,3 +0,0 @@
--- Fix search_path security warnings for functions
-ALTER FUNCTION public.validate_lead_data() SET search_path = public;
-ALTER FUNCTION public.cleanup_rate_limits() SET search_path = public;
